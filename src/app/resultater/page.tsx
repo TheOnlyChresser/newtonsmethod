0656209127db794b455b4b847d88114c433b7714
TheOnlyChresser <120821352+TheOnlyChresser@users.noreply.github.com>
2025-07-25 04:41:53 +0200

    debugging

--- src/app/resultater/page.tsx
+++ src/app/resultater/page.tsx
@@ -24,6 +24,7 @@ export default function Index() {
         setYarray(storedYarray);
         const storedPointarray = JSON.parse(localStorage.getItem("pointarray") || "null");
         setPointarray(storedPointarray);
+        console.log(pointarray);
     }, []);
     return (
         <>
